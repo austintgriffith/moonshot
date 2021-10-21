50fbc84c19133e9b79383e63ea08af52f51fc2e5
izayl <izayl@163.com>
2021-10-21 10:30:01 +0800

    feat: add workstream filter

--- packages/react-app/gulpfile.js
+++ packages/react-app/gulpfile.js
@@ -33,7 +33,9 @@ gulp.task('notion-sync', async () => {
   const projectsDB = await getNotionDatabase('70c8e70f88084a248015d2313f755519')
   const membersDB = await getNotionDatabase('856ad31d01c848448c34631c7b795d27')
   
-  const members = membersDB.map(page => ({ name: getPageProperty(page, 'Name') }))
+  const members = membersDB
+    .filter(page => getPageProperty(page, "Workstreams", "multi_select").includes("Moonshot Collective"))
+    .map(page => ({ name: getPageProperty(page, "Name") }));
   const projects = projectsDB.map(page => {
     return { name: getPageProperty(page, 'Name'), link: getPageProperty(page, 'Github') }
   })
